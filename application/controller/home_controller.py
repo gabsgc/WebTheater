7602db6e0b0982716d74141dc51442f8825f9f04
gabsgc <gabriellagarccia@gmail.com>
2021-04-13 13:04:22 -0300

    add assets + up base + create home, partials

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -1,6 +1,13 @@
 from application import app
 from flask import render_template, request
+from application.model.entity.video import Video
+from application.model.entity.categoria import Categoria
+from application.model.dao.categoria_dao import CategoriaDAO
 
 @app.route('/')
 def home():
-    return render_template('base.html')
+    """
+    categorias = CategoriaDAO()
+    categorias.listarTodos()
+    """
+    return render_template('home.html')
